f68e056f5f5b8a071e782be780f9b5cd94e1f64d
chaery-prog <fortpoon@gmail.com>
2025-03-18 10:02:22 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ColorChange from './components/ColorChange'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ColorChange/>
+      </div>
+    </>
+  )
+}
+
+export default App
